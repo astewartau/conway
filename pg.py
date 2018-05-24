90e73f87a5ee9fe6c32e589518f9523b7c38b544
Ashley Stewart <a.stewart.au@gmail.com>
2018-05-24 22:28:06 +1000

    Added controls

--- pg.py
+++ pg.py
@@ -4,8 +4,8 @@ import pygame as pg
 from conway import *
     
 def main():
-    cell_size = 5
-    grid_size = 270, 200
+    cell_size = 10
+    grid_size = 80, 50
     grid = build_life_grid(*grid_size)
 
     pg.display.init()
@@ -14,28 +14,46 @@ def main():
     surface = pg.display.get_surface()
     clock = pg.time.Clock()
     close = False
+    pause = False
+    one_frame = False
+    speed = 15
     while not close:
-        surface.fill((20, 20, 20))
-        y_pos = 0
-        for row in grid:
-            x_pos = 0
-            for cell in row:
-                if cell:
-                    surface.fill(
-                        (125, 125, 125),
-                        pg.Rect(x_pos, y_pos, cell_size, cell_size)
-                    )
-                x_pos += cell_size
-            y_pos += cell_size
-        pg.display.flip()
-        conway(grid)
-        clock.tick(50)
+        if not pause or one_frame:
+            surface.fill((20, 20, 20))
+            y_pos = 0
+            for row in grid:
+                x_pos = 0
+                for cell in row:
+                    if cell:
+                        surface.fill(
+                            (125, 125, 125),
+                            pg.Rect(x_pos, y_pos, cell_size, cell_size)
+                        )
+                    x_pos += cell_size
+                y_pos += cell_size
+            pg.display.flip()
+            conway(grid)
+            if one_frame:
+                one_frame = False
+        clock.tick(speed)
         events = pg.event.get()
         for event in events:
             if event.type == pg.QUIT:
                 pg.quit()
                 close = True
                 break
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if event.button == 3:
+                    one_frame = True
+                elif event.button == 1:
+                    pause = not pause
+                elif event.button == 4:
+                    speed+=1
+                elif event.button == 5:
+                    speed-=1
+                if speed <= 0:
+                    speed = 1
+                
         
         
         
